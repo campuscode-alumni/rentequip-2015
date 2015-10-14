5dcd915169f8c1670635090ddff5d98f26cdb150
David/Mateus <campuscode@campuscode.com.br>
2015-10-13 22:22:15 -0300

    add rental period to prices

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151008235533) do
+ActiveRecord::Schema.define(version: 20151014003214) do
 
   create_table "contracts", force: :cascade do |t|
     t.string   "customer"
@@ -54,12 +54,14 @@ ActiveRecord::Schema.define(version: 20151008235533) do
     t.string   "equipment"
     t.string   "time"
     t.decimal  "total"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
     t.integer  "equipment_id"
+    t.integer  "rental_period_id"
   end
 
   add_index "prices", ["equipment_id"], name: "index_prices_on_equipment_id"
+  add_index "prices", ["rental_period_id"], name: "index_prices_on_rental_period_id"
 
   create_table "rental_periods", force: :cascade do |t|
     t.string   "description"
