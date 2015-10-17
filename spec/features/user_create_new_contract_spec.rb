37a826fc3e8d7460ae107719bc432b6cf27861ed
wesleysoares <wesley.ssoares1@gmail.com>
2015-10-17 00:22:13 -0300

    Add payment methods in contract

--- spec/features/user_create_new_contract_spec.rb
+++ spec/features/user_create_new_contract_spec.rb
@@ -16,7 +16,7 @@ feature 'User create a new contract' do
     select customer.name, from: 'Cliente'
     check equipment.name
     select rental_period.description, from: 'Prazo de locação'
-    fill_in 'Forma de pagamento', with: 'Á vista'
+    choose 'Á vista'
     fill_in 'Endereço de entrega', with: 'Av Paulista, 1985'
 
     click_on "Criar Contrato"
